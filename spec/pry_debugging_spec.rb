be0aa992efeeee584b08a7300087fe4cab9811c3
Mike Esposito <mikeesposito6852@gmail.com>
2020-05-17 09:13:56 -0700

    Debug pry_debugging

--- spec/pry_debugging_spec.rb
+++ spec/pry_debugging_spec.rb
@@ -1,5 +1,5 @@
 require 'pry'
-require './lib/pry_debugging.rb'
+require '../lib/pry_debugging.rb'
 
 describe "#plus_two" do 
 	it "takes in a number as an argument and returns the sum of that number and 2" do
